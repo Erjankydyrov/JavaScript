5c1f11844bd031ac6f5ebdac07ae1a1d186e3d37
Erjankydyrov <Erjankydyrov29@gmail.com>
2021-01-15 14:22:07 +0600

    added console.log("London is the capital of GB");

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let first = "Shrek";
-let second = first; 
-let third = second;
-let fourth = third;
-let fifth = fourth;
-console.log(fifth);
+// let first = "Shrek";
+// let second = first; 
+// let third = second;
+// let fourth = third;
+// let fifth = fourth;
+// console.log(fifth);
+console.log("London is the capital of GB");
